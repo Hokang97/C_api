a9ff815a4c137af179a70c80424fe822da8bdbe4
Hokang97 <aaa7143874@naver.com>
2020-07-30 23:02:06 +0900

    win api practice

---
+++ 28_리소스/28_리소스/resource.h
@@ -0,0 +1,24 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// 28_리소스.rc에서 사용되고 있습니다.
+//
+#define IDS_GAME_TITLE                  101
+#define IDC_CURSOR1                     102
+#define IDC_CURSOR2                     103
+#define IDC_CURSOR3                     104
+#define IDR_ACCELERATOR1                105
+#define IDR_COPY                        40001
+#define IDR_CUT                         40002
+#define IDR_PASTE                       40003
+#define IDR_UNDO                        40004
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_COMMAND_VALUE         40006
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
